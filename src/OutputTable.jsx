a3e54cfc6b604814f71d2f7de5db09bb4e264c58
Lewis Rafuse <47062950+rafusel@users.noreply.github.com>
2021-01-14 19:32:49 -0500

    Autocomplete for meal item form. (#10)

--- src/OutputTable.jsx
+++ src/OutputTable.jsx
@@ -21,7 +21,6 @@ const columns = [
 
 export default class OutputTable extends React.Component {
   getMealItemTotalByOrderer(orderer) {
-
     const itemTotal = this.props.receipt.mealItems.reduce((total, mealItem) => (
       mealItem.orderer === orderer ? total + parseFloat(extractFloatStringFromCurrencyString(mealItem.cost)) : total
     ), 0);
@@ -40,7 +39,7 @@ export default class OutputTable extends React.Component {
 
     const totalMinusShareEquallyValues = parseFloat(extractFloatStringFromCurrencyString(this.props.receipt.total)) - (
       parseFloat(extractFloatStringFromCurrencyString(this.props.receipt.deliveryFee))
-        + parseFloat(extractFloatStringFromCurrencyString(this.props.receipt.serviceFee))
+      + parseFloat(extractFloatStringFromCurrencyString(this.props.receipt.serviceFee))
     );
 
     const totals = [];
@@ -66,10 +65,15 @@ export default class OutputTable extends React.Component {
     const hasMealItems = this.props.receipt.mealItems.length;
     const totalMakesSense = totalFloat >= (this.getSubtotal() + serviceFeeFloat + deliveryFeeFloat);
     const mealCostsAreFloats = this.props.receipt.mealItems.every((mealItem) => (
-      parseFloat(extractFloatStringFromCurrencyString(mealItem.cost))
+      parseFloat(extractFloatStringFromCurrencyString(mealItem.cost)) >= 0
+    ));
+    const shareTotals = this.getShareTotals();
+    const shareTotalsHaveValues = shareTotals.every((shareTotal) => (
+      shareTotal.shareOfTotal >= 0 && shareTotal.orderer
     ));
 
-    if (receiptValuesAreFloats && hasOrderers && mealCostsAreFloats && hasMealItems && totalMakesSense) {
+    if (receiptValuesAreFloats && hasOrderers && mealCostsAreFloats
+      && hasMealItems && totalMakesSense && shareTotalsHaveValues) {
       return (
         <div style={{ padding: '30px 0 30px 0' }}>
           <Title>
@@ -91,10 +95,10 @@ export default class OutputTable extends React.Component {
           No splits to show
         </Title>
         <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            width: '100%',
-          }}
+          display: 'flex',
+          alignItems: 'center',
+          width: '100%',
+        }}
         >
           <span style={{ margin: 'auto', fontSize: '100px' }}>
             <FrownOutlined />
